7a7fd205151e3c08fa8e178c62e651ba3fc4d9ec
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-08-16 15:19:05 +0300

    working

--- on-boarding/todo_app/test/features/todo/data/data_source/local_data_source_test.dart
+++ on-boarding/todo_app/test/features/todo/data/data_source/local_data_source_test.dart
@@ -9,9 +9,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/core/errors/failure.dart';
 import 'package:todo_app/features/todo/data/models/task_model.dart';
 import 'package:todo_app/features/todo/domain/entities/task_entity.dart';
-// import 'package:todo_app/core/errors/failure.dart';
-// import 'package:todo_app/features/todo/data/models/task_model.dart';
-// import 'package:todo_app/features/todo/domain/entities/task_entity.dart';
 import 'package:todo_app/features/todo/data/data_source/local_data_source.dart';
 
 class MockSharedPreferences extends Mock implements SharedPreferences {}
